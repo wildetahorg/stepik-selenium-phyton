a00682afcd8717e56090659c84c02a75cea4edb6
kanikeva <kanikeva@4xxi.com>
2022-04-15 00:00:21 +0300

    Updated folder system for the course+lesson3.6 files

--- Lesson_3/Chrome_tests/Test_fixture12_lesson3_6_step3.py
+++ Lesson_3/Chrome_tests/Test_fixture12_lesson3_6_step3.py
@@ -3,15 +3,6 @@ from selenium import webdriver
 import time
 import math
 
-@pytest.fixture(scope="function")
-def browser():
-    print("\nstart browser for test..")
-    browser = webdriver.Chrome()
-    browser.implicitly_wait(30)
-    yield browser
-    print("\nquit browser..")
-    browser.quit()
-
 @pytest.mark.parametrize('link', ["236895", "236896", "236897", "236898", "236899", "236903", "236904", "236905"])
 def test_alien_message(browser, link):
         answer = str(math.log(int(time.time())))
---
+++ Lesson_3/Chrome_tests/conftest.py
@@ -0,0 +1,11 @@
+import pytest
+from selenium import webdriver
+
+@pytest.fixture(scope="function")
+def browser():
+    print("\nstart browser for test..")
+    browser = webdriver.Chrome()
+    browser.implicitly_wait(30)
+    yield browser
+    print("\nquit browser..")
+    browser.quit()
