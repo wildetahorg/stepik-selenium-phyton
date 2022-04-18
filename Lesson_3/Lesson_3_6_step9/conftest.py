432c71d221236cca3d5423a4cf3a9b4e9cdd2c85
kanikeva <kanikeva@4xxi.com>
2022-04-18 12:47:38 +0300

    Change the conftest.py

--- Lesson_3/Lesson_3_6_step9/conftest.py
+++ Lesson_3/Lesson_3_6_step9/conftest.py
@@ -13,18 +13,22 @@ def browser(request):
     browser_name = request.config.getoption("browser_name")
     user_language = request.config.getoption("language")
     browser = None
+
     if browser_name == "chrome":
         options = Options()
         options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
         print("\nstart chrome browser for test..")
         browser = webdriver.Chrome(options=options)
+
     elif browser_name == "firefox":
         fp = webdriver.FirefoxProfile()
         fp.set_preference("intl.accept_languages", user_language)
         print("\nstart firefox browser for test..")
         browser = webdriver.Firefox(firefox_profile=fp)
+
     else:
         raise pytest.UsageError("--browser_name should be chrome or firefox")
+        
     yield browser
     print("\nquit browser..")
     browser.quit()
